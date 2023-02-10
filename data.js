1dcc9c281acb57f2e2bab94a9d05a80a63c1dbd4
Savagepvppanda <98602433+Savagepvppanda@users.noreply.github.com>
2023-02-10 11:20:47 -0500

    Update data.js

---
+++ data.js
@@ -0,0 +1,152 @@
+let songs = [
+    {
+        name: 'Blinding Lights',
+        path: 'musics/The Weeknd - Blinding Lights.mp3',
+        artist: 'The Weeknd',
+        cover: 'images/The Weeknd - Blinding Lights.jpg'
+    },
+    {
+        name: 'Kiss Me More (explicit)',
+        path: 'musics/Doja Cat - Kiss Me More (explicit).mp3',
+        artist: 'Doja Cat',
+        cover: 'images/Doja Cat - Kiss Me More (explicit).jpg'
+    },
+    {
+        name: 'Drivers License',
+        path: 'musics/Drivers License - Olivia Rodrigo (explicit).mp3',
+        artist: 'Olivia Rodrigo',
+        cover: 'images/Drivers License - Olivia Rodrigo (explicit).jpg'
+    },
+    {
+        name: 'Montero (explicit)',
+        path: 'musics/Lil Nas X - Montero (explicit).mp3',
+        artist: 'Lil Nas X',
+        cover: 'images/Lil Nas X - Montero (explicit).jpg'
+    },
+    {
+        name: 'Heat Waves',
+        path: 'musics/Glass Animals - Heat Waves.mp3',
+        artist: 'Glass Animals',
+        cover: 'images/Glass Animals - Heat Waves.jpg'
+    },
+    {
+        name: 'Wicked Games',
+        path: 'musics/The Weeknd - Wicked Games.mp3',
+        artist: 'The Weeknd',
+        cover: 'images/The Weeknd - Wicked Games.jpg'
+    },
+    {
+        name: 'Peaches',
+        path: 'musics/Justin Bieber - Peaches.mp3',
+        artist: 'Justin Bieber',
+        cover: 'images/Justin Bieber - Peaches.jpg'
+    },
+    {
+        name: 'Levitating (DaBaby Remix) (explicit)',
+        path: 'musics/Dua Lipa - Levitating (DaBaby Remix) (explicit).mp3',
+        artist: 'Dua Lipa',
+        cover: 'images/Dua Lipa - Levitating (DaBaby Remix) (explicit).jpg'
+    },
+    {
+        name: 'Industry Baby (explicit)',
+        path: 'musics/Lil Nas X - Industry Baby (explicit).mp3',
+        artist: 'Lil Nas X',
+        cover: 'images/Lil Nas X - Industry Baby (explicit).jpg'
+    },
+    {
+        name: 'Astronaut in the Ocean(explicit)',
+        path: 'musics/Masked Wolf - Astronaut in the Ocean (explicit).mp3',
+        artist: 'Masked Wolf',
+        cover: 'images/Masked Wolf - Astronaut in the Ocean (explicit).jpg'
+    },
+        {
+        name: 'Stay (explicit)',
+        path: 'musics/The Kid Laroi and Justin Bieber - Stay (explicit).mp3',
+        artist: 'The Kid Laroi and Justin Bieber',
+        cover: 'images/The Kid Laroi and Justin Bieber - Stay (explicit).jpg'
+    },
+    {
+        name: 'Light Switch',
+        path: 'musics/Charlie Puth - Light Switch.mp3',
+        artist: 'Charlie Puth',
+        cover: 'images/Charlie Puth - Light Switch.jpg'
+    },
+    {
+        name: 'Ghost Rule',
+        path: 'musics/Ghost Rule - DECO_27.mp3',
+        artist: 'DECO*27',
+        cover: 'images/Ghost Rule - DECO_27.jpg'
+    },
+    {
+        name: 'Pet',
+        path: 'musics/A Perfect Circle - Pet.mp3',
+        artist: 'A Perfect Circle',
+        cover: 'images/A Perfect Circle - Pet.jpg'
+    },
+    {
+        name: 'Tentative',
+        path: 'musics/Tentative - System of a Down.mp3',
+        artist: 'System of a Down',
+        cover: 'images/Tentative - System of a Down.jpg'
+    },
+    {
+        name: 'Yesterday',
+        path: 'musics/Beatles singles-yesterday.mp3',
+        artist: 'Beatles singles',
+        cover: 'images/Beatles singles-yesterday.jpg'
+    },
+    {
+        name: 'Looks',
+        path: 'musics/Steve Lacy - Looks.mp3',
+        artist: 'Steve Lacy',
+        cover: 'images/Steve Lacy - Looks.jpg'
+    },
+    {
+        name: 'Good 4 U (explicit)',
+        path: 'musics/Good 4 U (explicit).mp3',
+        artist: 'Olivia Rodrigo',
+        cover: 'images/Good 4 U (explicit).jpg'
+    },
+    {
+        name: 'Roman s Revenge',
+        path: 'musics/Roman s Revenge.mp3',
+        artist: 'Nicki Minaj',
+        cover: 'images/Roman s Revenge.jpg'
+    },
+    {
+        name: 'Haan Main Galat',
+        path: 'musics/Haan Main Galat.mp3',
+        artist: 'Love Aaj Kal',
+        cover: 'images/Haan Main Galat.jpg'
+    },
+    {
+        name: 'I cant handle change',
+        path: 'musics/I cant handle change.mp3',
+        artist: 'Roar',
+        cover: 'images/I cant handle change.jpg'
+    },
+    {
+        name: 'Hot Tub(ft. T-Pain)',
+        path: 'musics/Yung Gravy & Dillon Francis - Hot Tub (ft. T-Pain).mp3',
+        artist: 'Yung Gravy & Dillon Francis',
+        cover: 'images/Yung Gravy & Dillon Francis - Hot Tub (ft. T-Pain).jpg'
+    },
+    {
+        name: 'Love Shack',
+        path: 'musics/The B-52 - Love Shack.mp3',
+        artist: "The B-52's",
+        cover: 'images/The B-52 - Love Shack.jpg'
+    },
+    {
+        name: 'Over',
+        path: 'musics/Over - Lucky Day.mp3',
+        artist: 'Lucky Day',
+        cover: 'images/Over - Lucky Day.jpg'
+    },
+    {
+        name: 'Dollar Cost',
+        path: 'musics/Dollar-Cost-Kendrick-Lamar.mp3',
+        artist: 'Kendrick Lamar',
+        cover: 'images/Dollar-Cost-Kendrick-Lamar.jpg'
+    },
+]
